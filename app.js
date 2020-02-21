60539fabe7734fb44c87b45617aca00932317188
Jakob <jakob@folkdatorn.se>
2020-02-21 18:09:01 +0100

    More advanced version

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
+
+function createItemElement (text) {
+  const containerElement = document.createElement('div')
+  const textElement = document.createElement('p')
+  const removeElement = document.createElement('div')
+
+  containerElement.classList.add('toDo')
+  textElement.textContent = text
+  removeElement.textContent = '✖'
+  removeElement.addEventListener('click', (e) => {
+    containerElement.parentNode.removeChild(containerElement)
+  })
+  removeElement.classList.add('remove')
+
+  containerElement.appendChild(textElement)
+  containerElement.appendChild(removeElement)
+  return containerElement
+}
+
 function add () {
   const input = document.querySelector('input')
   if (input.value === '') { return }
   const target = document.querySelector('#lista')
-  const element = document.createElement('p')
-
-  element.textContent = input.value
+  target.appendChild(createItemElement(input.value))
   input.value = ''
   input.focus()
-
-  target.appendChild(element)
 }
 
 document.querySelector('input').addEventListener('keyup', (e) => {
-  if (e.key == 'Enter') {
+  if (e.key === 'Enter') {
     add()
   }
 })
+
+// Uncomment this to debug
+// const input = document.querySelector('input')
+// input.value = 'test'
+// add()
